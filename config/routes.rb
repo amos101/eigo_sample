25e2af228bd7159e5d14a036f828e7f5ca6169f7
amos101 <64058677+amos101@users.noreply.github.com>
2021-07-11 16:53:01 +0900

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # apiはapiのnamespaceに作成する
+  namespace :api, format: 'json' do
+    post 'send' => 'send#send'
+  end
+
+  # getは全て、indexにredirect
+  get '*path' => 'home#index'
 end
