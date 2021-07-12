4cb974b8ed39c1c92fd59e7ee98c72d098035363
amos101 <64058677+amos101@users.noreply.github.com>
2021-07-12 14:51:07 +0900

    pg バージョン変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  namespace 'api' do
+    namespace 'v1' do
+      resources :posts
+    end
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # apiはapiのnamespaceに作成する
-  namespace :api, format: 'json' do
-    post 'send' => 'send#send'
-  end
+  # namespace :api, format: 'json' do
+  #   post 'send' => 'send#send'
+  # end
 
   # getは全て、indexにredirect
   get '*path' => 'home#index'
